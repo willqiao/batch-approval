26750bcc40bb7edd3c00959303e652bd591efb37
willqiao <will.qiao@gmail.com>
2018-12-06 09:44:37 -0600

    just a test

---
+++ src/main/java/com/qiaos/batchapproval/BatchApprovalApplication.java
@@ -0,0 +1,12 @@
+package com.qiaos.batchapproval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchApprovalApplication.class, args);
+	}
+}
