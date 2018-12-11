a08f6e88f4f70001d1f47cf3483f2a1a55ef31dc
willqiao <will.qiao@gmail.com>
2018-12-11 10:54:30 -0600

    first version

--- src/test/java/com/qiaos/batchapproval/BatchApprovalApplicationTests.java
+++ src/test/java/com/qiaos/batchapproval/BatchApprovalApplicationTests.java
@@ -1,16 +1,31 @@
 package com.qiaos.batchapproval;
 
+import java.sql.Timestamp;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.qiaos.batchapproval.model.ApprovalTask;
+import com.qiaos.batchapproval.rep.ApprovalTaskRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BatchApprovalApplicationTests {
+	@Autowired
+	ApprovalTaskRepository rep;
 
 	@Test
-	public void contextLoads() {
+	public void testContextLoads() {
+		System.out.println(rep.findAll());
+		ApprovalTask tt = new ApprovalTask();
+		tt.setTaskName("ffffff");
+		tt.setTaskOwner("will");
+		tt.setCreatedBy("aaaaaa");
+		tt.setCreatedTime(new Timestamp(System.currentTimeMillis()));
+		rep.save(tt);
 	}
 
 }
