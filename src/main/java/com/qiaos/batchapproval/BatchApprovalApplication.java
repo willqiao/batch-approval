d6367941acc77c01a4fcbe618ce0e0480e8c2f7a
osboxes <osboxes@osboxes>
2019-02-01 08:37:18 -0600

    this is changes

--- src/main/java/com/qiaos/batchapproval/BatchApprovalApplication.java
+++ src/main/java/com/qiaos/batchapproval/BatchApprovalApplication.java
@@ -1,9 +1,11 @@
 package com.qiaos.batchapproval;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.WebApplicationInitializer;
@@ -12,19 +14,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.qiaos.batchapproval.config.BeanConfig;
 import com.qiaos.batchapproval.model.ApprovalTask;
 
 
 @SpringBootApplication
 @RestController
 public class BatchApprovalApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+	@Autowired
+	private ConfigurableApplicationContext applicationContext;
 	
 	@RequestMapping(path="/hello", method=RequestMethod.GET , produces = "application/json")
 	public ApprovalTask hello(@RequestParam(name="first", defaultValue="will") String name) {
 		System.out.println("greeting api get...");
-		AnnotationConfigApplicationContext t = new AnnotationConfigApplicationContext(BeanConfig.class);
-		ApprovalTask g = t.getBean("greeting", ApprovalTask.class);
+//		AnnotationConfigApplicationContext t = new AnnotationConfigApplicationContext(BeanConfig.class);
+		ApprovalTask g = applicationContext.getBean("greeting", ApprovalTask.class);
 		g.setTaskId(1L);
 		g.setTaskName("Will");
 		g.setCreatedBy("test" + name);
