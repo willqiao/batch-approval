5439e9ea9cfc1e0b46ce48da37a65deee4207709
osboxes <osboxes@osboxes>
2019-02-02 10:13:41 -0600

    Add cmds for kafka

---
+++ start.sh
@@ -0,0 +1,2 @@
+/opt/kafka_2.11-2.1.0/bin/zookeeper-server-start.sh -daemon /opt/kafka_2.11-2.1.0/config/zookeeper.properties
+/opt/kafka_2.11-2.1.0/bin/kafka-server-start.sh -daemon /opt/kafka_2.11-2.1.0/config/server.properties
---
+++ stop.sh
@@ -0,0 +1,2 @@
+/opt/kafka_2.11-2.1.0/bin/kafka-server-stop.sh
+/opt/kafka_2.11-2.1.0/bin/zookeeper-server-stop.sh
