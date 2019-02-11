f55ff542efebe64cb230bf9cc1baa4f3badadfd8
will <will.qiao@gmail.com>
2019-02-11 10:32:12 -0600

    add static prod build to repo

---
+++ batch-ui/my-react-app/build/precache-manifest.2b3b4ca8be4669a8320f0a6940108126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7d1fe107f0750818fc4e",
+    "url": "/static/js/main.7d1fe107.chunk.js"
+  },
+  {
+    "revision": "58a82e60ea440236b099",
+    "url": "/static/js/1.58a82e60.chunk.js"
+  },
+  {
+    "revision": "7d1fe107f0750818fc4e",
+    "url": "/static/css/main.7ab94a26.chunk.css"
+  },
+  {
+    "revision": "d05055bdf6703436c9e6c3fe7b1b0b98",
+    "url": "/index.html"
+  }
+];
