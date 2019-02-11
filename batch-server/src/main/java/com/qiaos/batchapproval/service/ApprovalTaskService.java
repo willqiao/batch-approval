0872591667d52807afa377f67c30972e40e003ce
will <will.qiao@gmail.com>
2019-02-11 11:23:37 -0600

    Remove absolute path

--- batch-server/src/main/java/com/qiaos/batchapproval/service/ApprovalTaskService.java
+++ batch-server/src/main/java/com/qiaos/batchapproval/service/ApprovalTaskService.java
@@ -26,7 +26,7 @@ public class ApprovalTaskService {
 	@Cacheable("tests")
 	public ArrayList<ApprovalTask> getTasks() {
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ docs/precache-manifest.2f5f517236fc59c5751aa179a02f78db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9886ce0512fc31a1b273",
+    "url": "./static/js/main.9886ce05.chunk.js"
+  },
+  {
+    "revision": "58a82e60ea440236b099",
+    "url": "./static/js/1.58a82e60.chunk.js"
+  },
+  {
+    "revision": "9886ce0512fc31a1b273",
+    "url": "./static/css/main.7ab94a26.chunk.css"
+  },
+  {
+    "revision": "2fc920f691e820b1287770f9514635ab",
+    "url": "./index.html"
+  }
+];
